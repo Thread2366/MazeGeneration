ce9c8028fd2298143ed813b2a87da8007cb323b7
Thread2366 <39843824+Thread2366@users.noreply.github.com>
2019-01-10 11:14:59 +0300

    Add files via upload

---
+++ Code.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Maze
+{
+    class Code
+    {
+        public enum CellMeaning
+        {
+            Unvisited,
+            Wall,
+            Passage,
+            Visited,
+            Carver
+        };
+
+        public enum Directions
+        {
+            North,
+            East,
+            South,
+            West
+        };
+
+        static Point[] steps = new Point[4]
+        {
+            new Point(0, -1),
+            new Point(1, 0),
+            new Point(0, 1),
+            new Point(-1, 0)
+        };
+
+        public static int[,] InitializeGrid(int[,] grid)
+        {
+            int[,] result = grid;
+            for (int i = 0; i < result.GetLength(0); i++)
+            {
+                for (int j = 0; j < result.GetLength(1); j++)
+                {
+                    if (i % 2 == 1 && j % 2 == 1)
+                    {
+                        result[i, j] = (int)CellMeaning.Unvisited;
+                    }
+                    else result[i, j] = (int)CellMeaning.Wall;
+                }
+            }
+            return result;
+        }
+
+        public static Bitmap Render(Color color, Bitmap maze, int leftPixel, int rightPixel,
+                                                              int topPixel,  int bottomPixel)
+        {
+            for (int x = leftPixel; x < rightPixel; x++)
+            {
+                for (int y = topPixel; y < bottomPixel; y++)
+                {
+                    maze.SetPixel(x, y, color);
+                }
+            }
+            return maze;
+        }
+        
+        public static Bitmap Render(int[,] grid, Bitmap maze, int cellSize, int left, int right, int top, int bottom)
+        {
+            for (int i = left; i < right; i++)
+            {
+                for (int j = top; j < bottom; j++)
+                {
+                    for (int x = i * cellSize; x < (i + 1) * cellSize; x++)
+                    {
+                        for (int y = j * cellSize; y < (j + 1) * cellSize; y++)
+                        {
+                            switch (grid[i, j])
+                            {
+                                case (int)CellMeaning.Unvisited:
+                                    maze.SetPixel(x, y, Color.LightGray);
+                                    break;
+                                case (int)CellMeaning.Wall:
+                                    maze.SetPixel(x, y, Color.Black);
+                                    break;
+                                case (int)CellMeaning.Passage:
+                                    maze.SetPixel(x, y, Color.White);
+                                    break;
+                                case (int)CellMeaning.Visited:
+                                    maze.SetPixel(x, y, Color.LightPink);
+                                    break;
+                                case (int)CellMeaning.Carver:
+                                    maze.SetPixel(x, y, Color.LightGreen);
+                                    break;
+                                default:
+                                    maze.SetPixel(x, y, Color.Black);
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+            return maze;
+        }
+
+        public static Tuple<Point, Directions, int> MoveTo(Bitmap maze, Point location, int cellSize, Directions direction)
+        {
+            direction = (Directions)((int)(direction + 3) % 4);
+            int rotation = 3;
+            for (int i = 0; i < 4; i++)
+            {
+                bool notWall = maze.GetPixel(location.X + steps[(int)direction].X * cellSize,
+                                             location.Y + steps[(int)direction].Y * cellSize).ToArgb() 
+                                             != Color.Black.ToArgb();
+                if (notWall)
+                {
+                    location.X += (steps[(int)direction].X * cellSize);
+                    location.Y += (steps[(int)direction].Y * cellSize);
+                    break;
+                }
+                else
+                {
+                    direction = (Directions)((int)(direction + 1) % 4);
+                    rotation = (rotation + 1) % 4;
+                }
+            }
+            return Tuple.Create(location, direction, rotation);
+        }
+
+        public static Point CarveTo(int[,] grid, Point currentLocation)
+        {
+            Random rnd = new Random();
+            var shuffledDirections = new int[] { 0, 1, 2, 3 }.OrderBy(a => rnd.Next()).ToArray();
+            var destination = currentLocation;
+            for (int i = 0; i < 4; i++)
+            {
+                bool inBounds = currentLocation.X + steps[shuffledDirections[i]].X * 2 >= 0 &&
+                                currentLocation.X + steps[shuffledDirections[i]].X * 2 < grid.GetLength(0) &&
+                                currentLocation.Y + steps[shuffledDirections[i]].Y * 2 >= 0 &&
+                                currentLocation.Y + steps[shuffledDirections[i]].Y * 2 < grid.GetLength(1);
+                bool notVisited = false;
+                if (inBounds)
+                    notVisited = grid[currentLocation.X + steps[shuffledDirections[i]].X * 2,
+                                      currentLocation.Y + steps[shuffledDirections[i]].Y * 2] == 0;
+                if (inBounds && notVisited)
+                {
+                    destination.X = currentLocation.X + steps[shuffledDirections[i]].X * 2;
+                    destination.Y = currentLocation.Y + steps[shuffledDirections[i]].Y * 2;
+                    Mark(grid, currentLocation, destination);
+                    break;
+                }
+            }
+            return destination;
+        }
+
+        public static void Mark(int[,] grid, Point currentLocation, Point destination)
+        {
+            int left = Math.Min(currentLocation.X, destination.X);
+            int right = Math.Max(currentLocation.X, destination.X);
+            int top = Math.Min(currentLocation.Y, destination.Y);
+            int bottom = Math.Max(currentLocation.Y, destination.Y);
+            int destinationValue = grid[destination.X, destination.Y];
+            for (int x = left; x <= right; x++)
+            {
+                for (int y = top; y <= bottom; y++)
+                {
+                    if (destinationValue == (int)CellMeaning.Visited)
+                        grid[x, y] = (int)CellMeaning.Passage;
+                    else
+                        grid[x, y] = (int)CellMeaning.Visited;
+                }
+            }
+            grid[destination.X, destination.Y] = (int)CellMeaning.Carver;
+        }
+
+        public static Bitmap DrawSchematicMouse(Bitmap mouse)
+        {
+            for (int y = 0; y < mouse.Height; y++)
+            {
+                for (int x = 0; x < mouse.Width; x++)
+                {
+                    if (x >= y && x < mouse.Width - y)
+                        mouse.SetPixel(x, y, Color.LightBlue);
+                    else
+                        mouse.SetPixel(x, y, Color.Gray);
+                }
+            }
+            return mouse;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maze
+{
+    public partial class Form1 : Form
+    {
+        public static Stack<Point> backtrack = new Stack<Point>();
+
+        public static Point CarveFrom(Point point, Bitmap maze, int[,] grid, int cellSize)
+        {
+            var nextPoint = Code.CarveTo(grid, point);
+            if (nextPoint != point)
+            {
+                backtrack.Push(point);
+            }
+            else
+            {
+                nextPoint = backtrack.Pop();
+                Code.Mark(grid, point, nextPoint);
+            }
+            int renderLeftBorder = Math.Min(point.X, nextPoint.X);
+            int renderRightBorder = Math.Max(point.X, nextPoint.X) + 1;
+            int renderTopBorder = Math.Min(point.Y, nextPoint.Y);
+            int renderBottomBorder = Math.Max(point.Y, nextPoint.Y) + 1;
+            maze = Code.Render(grid, maze, cellSize, renderLeftBorder,
+                                                     renderRightBorder,
+                                                     renderTopBorder,
+                                                     renderBottomBorder);
+            return nextPoint;
+        }
+
+        Bitmap maze = new Bitmap(1125, 675);
+        int cellSize = 0;
+
+        PictureBox pictureBox2 = new PictureBox()
+        {
+            Visible = false
+        };
+        Bitmap mouse;
+        Point exit;
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = comboBox1.Items.Count - 1;
+            pictureBox1.Controls.Add(pictureBox2);
+        }
+
+        int[,] grid = new int[375, 225];
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cellSize = Convert.ToInt32(comboBox1.SelectedItem);
+            grid = new int[1125 / cellSize, 675 / cellSize];
+            grid = Code.InitializeGrid(grid);
+            maze = Code.Render(grid, maze, cellSize, 0, grid.GetLength(0), 0, grid.GetLength(1));
+            pictureBox1.Image = maze;
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            comboBox1.Enabled = false;
+            Point carvePoint = new Point(1, 1);
+            backtrack.Push(carvePoint);
+            while (backtrack.Count != 0)
+            {
+                carvePoint = CarveFrom(carvePoint, maze, grid, cellSize);
+                pictureBox1.Refresh();
+            }
+            grid[1, 1] = (int)Code.CellMeaning.Passage;
+            maze = Code.Render(grid, maze, cellSize, 1, 2, 1, 2);
+
+            mouse = new Bitmap(cellSize, cellSize);
+            mouse = Code.DrawSchematicMouse(mouse);
+            pictureBox2.Height = cellSize;
+            pictureBox2.Width = cellSize;
+            pictureBox2.Image = mouse;
+            exit = new Point(cellSize, cellSize);
+
+            button2.Enabled = false;
+            pictureBox1.Enabled = true;
+            MessageBox.Show("Укажите на лабиринте местоположение выхода и мыши.");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var currentDirection = Code.Directions.North;
+            while (maze.GetPixel(pictureBox2.Location.X, pictureBox2.Location.Y).ToArgb() != Color.LightGreen.ToArgb())
+            {
+                var tuple = Code.MoveTo(maze, pictureBox2.Location, cellSize, currentDirection);
+                pictureBox2.Location = tuple.Item1;
+                currentDirection = tuple.Item2;
+                pictureBox2.Image.RotateFlip((RotateFlipType)tuple.Item3);
+                pictureBox1.Refresh();
+                pictureBox2.Refresh();
+            }
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (maze.GetPixel(e.X, e.Y).ToArgb() != Color.Black.ToArgb())
+            {
+                maze = Code.Render(Color.White, maze, exit.X, exit.X + cellSize, exit.Y, exit.Y + cellSize);
+                pictureBox2.Location = exit;
+                exit.X = e.X / cellSize * cellSize;
+                exit.Y = e.Y / cellSize * cellSize;
+                pictureBox2.Visible = true;
+                maze = Code.Render(Color.LightGreen, maze, exit.X, exit.X + cellSize, exit.Y, exit.Y + cellSize);
+                pictureBox1.Image = maze;
+                button3.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Здесь стена!");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            maze = new Bitmap(1125, 675);
+            pictureBox1.Image = maze;
+            comboBox1.Enabled = true;
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            pictureBox1.Enabled = false;
+            pictureBox2.Visible = false;
+        }
+    }
+}
